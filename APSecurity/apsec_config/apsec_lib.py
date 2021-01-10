9022ab48f5382cbe9cafd7ec6d40d12552e04914
poponealex <74072635+poponealex@users.noreply.github.com>
2021-01-10 16:30:29 +0100

    Update apsec_lib.py

--- APSecurity/apsec_config/apsec_lib.py
+++ APSecurity/apsec_config/apsec_lib.py
@@ -1,14 +1,14 @@
 from ast import literal_eval
 import time
 
-class Color:
-    """ANSI color codes.
 
-    Args:
-        - FAIL: bold red
-        - INFORMATION: bold blue
-        - TITLE: bold cyan
-        - END: end point of the coloration
+class Color:
+    """ANSI COLORS
+
+    Color.FAIL --> bold red
+    Color.INFORMATION --> bold blue
+    Color.TITLE --> bold cyan
+    Color.END
     """
 
     FAIL = "\033[91m\033[1m"
@@ -24,35 +24,28 @@ def input_value_satisfying_condition(
 ):
     """Validating user input by predicate.
 
-    Vars:
+    Parameters:
         - prompt: message displayed when prompting for user input.
-        - is_valid: lambda function to verify a condition.
+        - is_valid: lambda function to check the validity of the input.
         - failure_description: message displayed when is_valid()'s condition is not met.
 
-    Returns:
-        - The value entered by the user if is_valid's condition is met and after confirmation by the user.
-        - If the is_valid condition fails failure_description is displayed
-        - If literal_eval fails an error message containing the raised exception.
+    If the is_valid condition fails failure_description is raised.
+    Returns the value entered after validation by is_valid.
     """
     while True:
         value = input(f"{Color.INFORMATION}{prompt}{Color.END}\n")
         try:
             value = literal_eval(value)
-        except Exception:
+        except:
             pass
         try:
-            if is_valid(value):
-                a = literal_eval(
-                    input(
-                        f"{Color.INFORMATION}Is this correct: {value} ? enter 1 to confirm, 0 to retry{Color.END}\n"
-                    )
-                )
-                if a == 1:
-                    return value
-            else:
-                print(f"{Color.FAIL}{failure_description}{Color.END}")
+            if not is_valid(value):
+                raise Exception(failure_description)
+            if literal_eval(input(f"{Color.INFORMATION}Is this correct: {value} ? enter 1 to confirm, 0 to retry{Color.END}\n")) == 1:
+                return value 
         except Exception as e:
-            print(f"{Color.FAIL}{e} was raised, try again{Color.END}")
+            print(f"{Color.FAIL}<{e}> was raised, try again{Color.END}")
+
 
 def get_date_time():
     """Current date and time
