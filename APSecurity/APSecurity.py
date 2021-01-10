e2d369b6ab00aed0c738e071ea5a82af87ae3f73
poponealex <74072635+poponealex@users.noreply.github.com>
2021-01-10 17:52:40 +0100

    update

--- APSecurity/APSecurity.py
+++ APSecurity/APSecurity.py
@@ -9,6 +9,21 @@ from pathlib import Path
 from email.message import EmailMessage
 from apsec_config.apsec_lib import *
 
+TWO_MINUTES = 120
+THIRTY_MINUTES = 1800
+ONE_HOUR = 3600
+ONE_DAY = 86400
+MAX_CONSECUTIVE_ALERTS = 15
+
+def close(signum="", frame="", process_started=True, message=""):
+    print(f"{message}\n{Color.INFORMATION}QUITTING..{Color.END}\n")
+    if process_started:
+        send_text_message("SECURITY SYSTEM is OFF")
+        send_email(subject="SECURITY SYSTEM OFF", msg="SECURITY SYSTEM is OFF")
+        log._add_event(type="end")
+        gpio.cleanup()
+    sys.exit(0)
+signal.signal(signal.SIGINT, close)
 
 def check_load_config_file():
     """Checking and loading the configuration file.
@@ -17,120 +32,77 @@ def check_load_config_file():
     The config.json will be created.
 
     Always run 'APSecurity.py' from the APSecurity folder.
-
-    'var' (global variable) will store the object contained in the config.json as type(dict).
     """
-    if Path.cwd().name != "APSecurity":
-        print(
-            f"\n{Color.FAIL}Run 'APSecurity.py' from the 'APSecurity' directory.\nCurrent directory: {Path.cwd().name}.{Color.END}"
-        )
-        close(signal.SIGINT, 0, config=False)
+    if not Path.cwd().name == "APSecurity":
+        close(process_started=False, message=f"\n{Color.FAIL}Run 'APSecurity.py' from the 'APSecurity' directory.\nCurrent directory: {Path.cwd().name}.{Color.END}")
 
-    file = Path.cwd() / "apsec_config" / "config.json"
+    file_path = Path.cwd() / "apsec_config" / "config.json"
+    if not file_path.exists():
+        close(process_started=False, message=f"\n{Color.FAIL}Run the config.py script first.{Color.END}") 
 
-    if not file.exists():
-        print(f"\n{Color.FAIL}Run the config.py script first.{Color.END}")
-        close(signal.SIGINT, 0, config=False)
-    else:
-        global var
-        with open(file, "r") as f:
-            var = json.load(f)
+    with open(file_path, "r") as f:
+        return json.load(f)
 
-class Alerts:
-    """Alerts object is the security module that handles notifications and logs.
 
-    Public functions:
-        - send_email
-        - send_text_message
-
-    Private functions:
-         - _create_log
-         - _add_event_log
-    """
+class Logging:
+    """LOGGING MODULE"""
 
     def __init__(self):
+        self.path = Path.cwd() / "logs"
+        self.log = self.path / f"log_{get_date_time()}".replace(" ", "_").replace(":", "-")
         self._create_log()
         self.err_count = 0
 
-    def __repr__(self):
-        print("'Alerts' Security module")
-
     def _create_log(self):
-        """Create the log's file and folder."""
-
-        self.log_name = f"log_{get_date_time()}".replace(" ", "_").replace(":", "-")
-
-        path = f"{Path.cwd()}/logs"
-        p = Path(path)
-        if not p.exists():
-            p.mkdir()
-
-        file = Path.cwd() / "logs" / self.log_name
-        with open(file, "w") as f:
+        if not self.path.exists():
+            self.path.mkdir()
+        with open(self.log, "w") as f:
             f.write(f"\tstarted at: {get_date_time()}\n\n")
 
-    def _add_event_log(self, type="", nb=0, **msg):
-        """Add a log entry.
-
-        Args:
-            - type: A string that contains the log entry's type to create.
-            - *nb: The associated event's number.
-        """
-
-        t = get_date_time()
-        if type == "a":
-            self.content = f"{t}\nALERT nb: {nb}\n\n"
-        elif type == "p":
-            self.content = f"{t}\nPAUSE: {nb}\n\n"
-        elif type == "r":
-            self.content = f"{t}\nRESUME: {nb}\n\n"
-        elif type == "end":
-            self.content = f"\t{t}\n\tPROGRAM ENDED\n\n"
-        elif type == "err":
-            self.err_count += 1
-            self.content = f"\t{t}\n\tERROR: {self.err_count}\n\n"
-        else:
-            if msg:
-                self.content = f"\t{t}\n\t{msg}\n\n"
+    def _add_event(self, type="", nb="", msg=""):
+        with open(self.log, "a") as f:
+            if type == "a":
+                f.write(f"{get_date_time()}\nALERT nb: {nb}\n\n")
+            elif type == "p":
+                f.write(f"{get_date_time()}\nPAUSE: {nb} {msg}\n\n")
+            elif type == "r":
+                f.write(f"{get_date_time()}\nRESUME: {nb} {msg}\n\n")
+            elif type == "end":
+                f.write(f"\t{get_date_time()}\n\tPROGRAM ENDED\n\n")
+            elif type == "err":
+                self.err_count += 1
+                f.write(f"{get_date_time()}\nERROR: {self.err_count}\n{msg}\n\n")
+            elif msg:
+                f.write(f"{get_date_time()}\n{msg}\n\n")
             else:
-                self.content = f"\t{t}\n\tENTRY NOT SPECIFIED\n\n"
+                f.write(f"{get_date_time()}\nUNKNOWN ERROR\n\n")
 
-        file = Path.cwd() / "logs" / self.log_name
-        with open(file, "a") as f:
-            f.write(self.content)
+def send_email(subject="", msg=""):
+    try:
+        message = EmailMessage()
+        message["From"] = var["SENDER_NAME"]
+        message["To"] = var["MAIL_RECIPIENT"]
+        message["Subject"] = subject
+        message.set_content(msg)
 
-    def send_email(self, server, port, user, pwd, sender_name, recipient, subject, msg):
+        with smtplib.SMTP_SSL(var["MAIL_SERVER"], var["SERVER_PORT"], context=ssl._create_unverified_context()) as server:
+            server.login(var["MAIL_USER"], var["MAIL_PWD"])
+            server.send_message(msg)
+    except Exception as e:
+        log._add_event(type="err", msg=f"EMAIL ERROR: {e}")
 
-        self.server, self.port, self.user, self.pwd = server, port, user, pwd
-        self.msg = EmailMessage()
-        self.msg["From"] = sender_name
-        self.msg["To"] = recipient
-        self.msg["Subject"] = subject
-        self.msg.set_content(msg)
 
-        with smtplib.SMTP_SSL(
-            self.server, self.port, context=ssl._create_unverified_context()
-        ) as server:
-            try:
-                server.login(self.user, self.pwd)
-                server.send_message(self.msg)
-            except:
-                self._add_event_log(type="err")
-
-    def send_text_message(self, msg):
-
-        self.msg = msg.replace(" ", "%20")
-        self.u = f"{var['TXT_MSG_URL']}{self.msg}"
-        try:
-            urlopen(self.u, context=ssl._create_unverified_context())
-        except:
-            self._add_event_log(type="err")
-
+def send_text_message(message):
+    try:
+        url = f"{var['TXT_MSG_URL']}{message.replace(' ', '%20')}"
+        urlopen(url, context=ssl._create_unverified_context())
+    except Exception as e:
+        log._add_event(type="err", msg=f"TEXT MESSAGE ERROR: {e}")
 
 def setup():
     """Setting up the program.
 
-    It is important to note that the motion sensor will send HIGH level signals for about 80secs after being wired to the RPi.
+    Note that the motion sensor will send HIGH level signals for about 80secs after being wired to the RPi.
     """
     gpio.setmode(gpio.BOARD)
     gpio.setup(var["MOTION_DETECTOR_PIN"], gpio.IN)
@@ -139,18 +111,16 @@ def setup():
 
     sec = input_value_satisfying_condition(
         f"{Color.INFORMATION}Delay before starting the security system (in seconds)? {Color.END}",
-        is_valid=lambda value: value <= 1800,
+        is_valid=lambda value: value <= THIRTY_MINUTES,
         failure_description="should be at most 30 mn",
     )
 
     for a in range(sec, -1, -1):
-        sys.stdout.write(f"\r{Color.INFORMATION}{a}{Color.END}")
-        sys.stdout.flush()
+        sys.stdout.write(f" {Color.INFORMATION}{a}{Color.END} \r")
         if sec > 0:
             time.sleep(1)
 
     print(f"{Color.INFORMATION}\nSTARTING at {get_date_time()}!{Color.END}\n")
-
 
 def run():
     """Running the program.
@@ -159,84 +129,48 @@ def run():
     There is a 2min pause in between each alert.
     There is a 1 hour pause after 10 alerts.
     """
-
-    count_alerts = 0
-    count_pauses = 0
+    alerts_count = 0
+    pauses_count = 0
     start_time = time.time()
 
     while True:
-        if gpio.input(var["MOTION_DETECTOR_PIN"]) == gpio.HIGH:
-            t = get_date_time()
-            count_alerts += 1
-            print(f"{Color.INFORMATION}{t}{Color.END}")
-            print(f"{Color.FAIL}ALERT nb: {count_alerts}{Color.END}\n")
-            alert._add_event_log(type="a", nb=count_alerts)
-            alert.send_text_message(var["MSG"].format(x=get_date_time()))
-            alert.send_email(
-                var["MAIL_SERVER"],
-                var["SERVER_PORT"],
-                var["MAIL_USER"],
-                var["MAIL_PWD"],
-                var["SENDER_NAME"],
-                var["MAIL_RECIPIENT"],
-                var["MAIL_SUBJECT"],
-                var["MSG"].format(x=get_date_time()),
-            )
-            if count_alerts > 10:
-                count_pauses += 1
-                print(f"{Color.INFORMATION}PAUSING..{Color.END}")
-                alert._add_event_log(type="p", nb=count_pauses)
-                alert.send_text_message(var["MSG_BREAK"].format(x=get_date_time()))
-                alert.send_email(
-                    var["MAIL_SERVER"],
-                    var["SERVER_PORT"],
-                    var["MAIL_USER"],
-                    var["MAIL_PWD"],
-                    var["SENDER_NAME"],
-                    var["MAIL_RECIPIENT"],
-                    "break",
-                    var["MSG_BREAK"].format(x=get_date_time()),
-                )
-                time.sleep(3600)
-                count_alerts = 0
-                print(f"{Color.INFORMATION}RESUMING..{Color.END}\n")
-                alert._add_event_log(type="r", nb=count_pauses)
-                alert.send_text_message(var["MSG_RESUME"].format(x=get_date_time()))
-                alert.send_email(
-                    var["MAIL_SERVER"],
-                    var["SERVER_PORT"],
-                    var["MAIL_USER"],
-                    var["MAIL_PWD"],
-                    var["SENDER_NAME"],
-                    var["MAIL_RECIPIENT"],
-                    "resume",
-                    var["MSG_RESUME"].format(x=get_date_time()),
-                )
-            else:
-                print(f"{Color.INFORMATION}pausing..{Color.END}")
-                time.sleep(120)
-                print(f"{Color.INFORMATION}resuming..{Color.END}\n")
+        try:
+            if gpio.input(var["MOTION_DETECTOR_PIN"]) == gpio.HIGH:
+                alerts_count += 1
+                print(f"{Color.INFORMATION}{get_date_time()}{Color.END}")
+                print(f"{Color.FAIL}ALERT nb: {alerts_count}{Color.END}\n")
+                log._add_event(type="a", nb=alerts_count)
+                send_text_message(var["MSG"].format(x=get_date_time()))
+                send_email(subject=var["MAIL_SUBJECT"], msg=var["MSG"].format(x=get_date_time()))
+                if alerts_count > MAX_CONSECUTIVE_ALERTS:
+                    pauses_count += 1
+                    print(f"{Color.INFORMATION}PAUSING..{Color.END}")
+                    log._add_event(type="p", nb=pauses_count)
+                    send_text_message(var["MSG_BREAK"].format(x=get_date_time()))
+                    send_email(subject="break", msg=var["MSG_BREAK"].format(x=get_date_time()))
+                    time.sleep(ONE_HOUR)
+                    alerts_count = 0
+                    print(f"{Color.INFORMATION}RESUMING..{Color.END}\n")
+                    log._add_event(type="r", nb=pauses_count)
+                    send_text_message(var["MSG_RESUME"].format(x=get_date_time()))
+                    send_email(subject="resume", msg=var["MSG_RESUME"].format(x=get_date_time()))
+                else:
+                    print(f"{Color.INFORMATION}pausing..{Color.END}")
+                    log._add_event(type="p", msg="short 2min pause")
+                    time.sleep(TWO_MINUTES)
+                    print(f"{Color.INFORMATION}resuming..{Color.END}\n")
+                    log._add_event(type="p", msg="end of short pause")
 
-        if time.time() - start_time > 14400:
-            count_alerts = 0
-            start_time = time.time()
-            _add_event_log(msg=f"start time + alerts' count reseted at {get_date_time()}")
-
-
-def close(signum, frame, config=True):
-    print(f"{Color.INFORMATION}QUITTING..{Color.END}\n")
-    if config == True:
-        alert.send_text_message("SECURITY SYSTEM is OFF")
-        alert._add_event_log(type="end")
-        gpio.cleanup()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, close)
+            if time.time() - start_time > ONE_DAY:
+                alerts_count = 0
+                start_time = time.time()
+                log._add_event(msg=f"start time and alerts' count reseted at {get_date_time()}")
+        except Exception as e:
+            log._add_event(type="err", msg=f"{e}")
+            print(f"{Color.INFORMATION}<{e}> was raised!{Color.END}")
 
 if __name__ == "__main__":
-    check_load_config_file()
-    alert = Alerts()
-    while True:
-        setup()
-        run()
+    log = Logging()
+    var = check_load_config_file()
+    setup()
+    run()
--- APSecurity/apsec_config/config.py
+++ APSecurity/apsec_config/config.py
@@ -5,11 +5,10 @@ import json, re, signal, sys
 from pathlib import Path
 from apsec_lib import *
 
-def close(signum, frame):
+
+def close(signum="", frame=""):
     print(f"{Color.INFORMATION}QUITTING..{Color.END}\n")
     sys.exit(0)
-
-
 signal.signal(signal.SIGINT, close)
 
 
@@ -17,61 +16,62 @@ if Path.cwd().name != "apsec_config":
     print(
         f"\n{Color.FAIL}Run config.py from the 'apsec_config' directory -> 'APSecurity/apsec_config'.\nCurrent directory: {Path.cwd().name}.{Color.END}"
     )
-    close(signal.SIGINT, 0)
 
-print(f"{Color.TITLE}\n\t\tWELCOME to the sEcUrItY CoNfIg fIlE{Color.END}\n\n")
-var = {
-    "PROGRAM_NAME": "AP EaZey sEcuRiTy",
-    "MOTION_DETECTOR_PIN": input_value_satisfying_condition(
-        prompt="Enter the motion sensor's pin's number:",
-        is_valid=lambda value: type(value) == int and value >= 1 and value <= 40,
-    ),
-    "MAIL_SERVER": input_value_satisfying_condition(
-        prompt="Enter the mail server's hostname:",
-        is_valid=lambda value: type(value) == str
-        and re.match(
-            r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~:()\"\"\[\]]{1,}[.][a-zA-Z0-9\[\]]{1,}$", value
+else:
+    print(f"{Color.TITLE}\n\t\tWELCOME to the sEcUrItY CoNfIg fIlE{Color.END}\n\n")
+    var = {
+        "PROGRAM_NAME": "AP EaZey sEcuRiTy",
+        "MOTION_DETECTOR_PIN": input_value_satisfying_condition(
+            prompt="Enter the motion sensor's pin's number:",
+            is_valid=lambda value: type(value) == int and value >= 1 and value <= 40,
         ),
-    ),
-    "SERVER_PORT": input_value_satisfying_condition(
-        prompt="Enter the mail server's PORT number:",
-        is_valid=lambda value: type(value) == int,
-    ),
-    "MAIL_USER": input_value_satisfying_condition(
-        prompt="Enter your username (email address):",
-        is_valid=lambda value: type(value) == str
-        and re.match(
-            r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~:()\"\"\[\]]{1,}[@][a-zA-Z0-9.!#$%&'*+-/=?^_`:{|}~()\"\"\[\]]{1,}[.][a-zA-Z0-9\[\]]{1,}$",
-            value,
+        "MAIL_SERVER": input_value_satisfying_condition(
+            prompt="Enter the mail server's hostname:",
+            is_valid=lambda value: type(value) == str
+            and re.match(
+                r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~:()\"\"\[\]]{1,}[.][a-zA-Z0-9\[\]]{1,}$", value
+            ),
         ),
-    ),
-    "MAIL_PWD": input_value_satisfying_condition(
-        prompt="Enter your password:",
-        is_valid=lambda value: type(value) == str,
-    ),
-    "SENDER_NAME": input_value_satisfying_condition(
-        prompt="Enter a custom sender's name:",
-        is_valid=lambda value: type(value) == str,
-    ),
-    "MAIL_RECIPIENT": input_value_satisfying_condition(
-        prompt="Enter the recipient's email for the alerts:",
-        is_valid=lambda value: type(value) == str
-        and re.match(
-            r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~:()\"\"\[\]]{1,}[@][a-zA-Z0-9.!#$%&'*+-/=?^_`:{|}~()\"\"\[\]]{1,}[.][a-zA-Z0-9\[\]]{1,}$",
-            value,
+        "SERVER_PORT": input_value_satisfying_condition(
+            prompt="Enter the mail server's PORT number:",
+            is_valid=lambda value: type(value) == int,
         ),
-    ),
-    "TXT_MSG_URL": input_value_satisfying_condition(
-        prompt="Enter your text message URL ending with the GET attribute 'msg=':",
-        is_valid=lambda value: type(value) == str and re.match(r".*[=]$", value),
-    ),
-    "MAIL_SUBJECT": "ALERT",
-    "MSG": "ALERT!!! MOTION SENSOR HAS DETECTED SOMEONE at {x}!",
-    "MSG_BREAK": "THE SECURITY SYSTEM IS PAUSING at {x}! It will resume in 2 hours.",
-    "MSG_RESUME": "THE SECURITY SYSTEM HAS RESUMED at {x}.",
-}
+        "MAIL_USER": input_value_satisfying_condition(
+            prompt="Enter your username (email address):",
+            is_valid=lambda value: type(value) == str
+            and re.match(
+                r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~:()\"\"\[\]]{1,}[@][a-zA-Z0-9.!#$%&'*+-/=?^_`:{|}~()\"\"\[\]]{1,}[.][a-zA-Z0-9\[\]]{1,}$",
+                value,
+            ),
+        ),
+        "MAIL_PWD": input_value_satisfying_condition(
+            prompt="Enter your password:",
+            is_valid=lambda value: type(value) == str,
+        ),
+        "SENDER_NAME": input_value_satisfying_condition(
+            prompt="Enter a custom sender's name:",
+            is_valid=lambda value: type(value) == str,
+        ),
+        "MAIL_RECIPIENT": input_value_satisfying_condition(
+            prompt="Enter the recipient's email for the alerts:",
+            is_valid=lambda value: type(value) == str
+            and re.match(
+                r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~:()\"\"\[\]]{1,}[@][a-zA-Z0-9.!#$%&'*+-/=?^_`:{|}~()\"\"\[\]]{1,}[.][a-zA-Z0-9\[\]]{1,}$",
+                value,
+            ),
+        ),
+        "TXT_MSG_URL": input_value_satisfying_condition(
+            prompt="Enter your text message URL ending with the GET attribute 'msg=':",
+            is_valid=lambda value: type(value) == str and re.match(r".*[=]$", value),
+        ),
+        "MAIL_SUBJECT": "ALERT",
+        "MSG": "ALERT!!! MOTION SENSOR HAS DETECTED SOMEONE at {x}!",
+        "MSG_BREAK": "THE SECURITY SYSTEM IS PAUSING at {x}! It will resume in 2 hours.",
+        "MSG_RESUME": "THE SECURITY SYSTEM HAS RESUMED at {x}.",
+    }
 
-with open("config.json", "w") as f:
-    json.dump(var, f)
+    with open("config.json", "w") as f:
+        json.dump(var, f)
 
-print(f"{Color.INFORMATION}\nDONE!\n\n{Color.END}")
+    print(f"{Color.INFORMATION}\nDONE!\n\n{Color.END}")
+close()
